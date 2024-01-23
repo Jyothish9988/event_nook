bbd84ac5521be0a8e9bbcd8c6dc301c9289a8f68
Jyothish S L <93032610+Jyothish9988@users.noreply.github.com>
2024-01-23 09:19:44 +0530

    22jan

--- event_nook/eventnook/doctype/bookings/bookings.py
+++ event_nook/eventnook/doctype/bookings/bookings.py
@@ -1,38 +1,23 @@
-# Assuming you have a custom DocType named Bookings
-
-import frappe
 from frappe.model.document import Document
-from frappe import _
+import frappe
 
 class Bookings(Document):
-    def validate(self, method=None):
-        # Check room availability before saving the booking
-        if self.is_booking_overlapping():
-            frappe.throw(_("Room is already booked for the specified date and time range."))
+    def on_submit(self):
+        self.validate_booking_time()
 
-    def is_booking_overlapping(self):
-        # Query existing bookings for the specified room and date
-        existing_bookings = frappe.get_all("Bookings",
+    def validate_booking_time(self):
+        # Check if the room is available for the given date and time period
+        booked_rooms = frappe.get_all(
+            "Bookings",
             filters={
                 "room_no": self.room_no,
+                "docstatus": 1,
+                "name": ("!=", self.name),
                 "date": self.date,
-                "name": ("!=", self.name) if self.name else None  # Exclude current booking for updates
+                "time_from": ("<", self.time_to),
+                "time_to": (">", self.time_from)
             },
-            fields=["name", "time_from", "time_to"]
         )
 
-        # Convert current booking time to comparable format (assuming HH:mm:ss)
-        current_time_from = frappe.utils.get_time(self.time_from)
-        current_time_to = frappe.utils.get_time(self.time_to)
-
-        # Check for overlapping time ranges
-        for booking in existing_bookings:
-            existing_time_from = frappe.utils.get_time(booking.time_from)
-            existing_time_to = frappe.utils.get_time(booking.time_to)
-
-            if (existing_time_from <= current_time_to and existing_time_to >= current_time_from):
-                # There is an overlap, room is already booked
-                return True
-
-        # No overlapping bookings, room is available
-        return False
+        if booked_rooms:
+            frappe.throw("The room is already booked for the selected date and time period.")
